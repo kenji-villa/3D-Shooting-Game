e7c1f3052dea82de25c7bc9a7e320952e430a2cf
Pixel_Devid <davis78villa@gmail.com>
2026-08-19 00:18:34 -0700

    Input

---
+++ src/Input.cpp
@@ -0,0 +1,45 @@
+#include "Input.h"
+#include "Globals.h"
+#include <GL/freeglut.h>
+#include <cstdlib>
+
+void input_passive_motion(int x, int y) {
+    (void)y;
+    // Lock aiming once a shot is in the air, same as the reference project.
+    if (g_gameState.mode == GameMode::Aiming) {
+        camera_handle_mouse(g_camera, x, g_windowWidth);
+    }
+}
+
+void input_key_up(unsigned char key, int x, int y) {
+    (void)x; (void)y;
+
+    switch (key) {
+        case 27: // esc
+            exit(0);
+            break;
+
+        case ' ': // fire
+            if (g_gameState.mode == GameMode::Aiming) {
+                Vec3 startPos = g_camera.eye + camera_aim_direction(g_camera) * 5.0f;
+                projectile_fire(g_projectile, g_gameState.currentWeapon, startPos, g_camera.yaw);
+                g_gameState.mode = GameMode::Firing;
+            }
+            break;
+
+        case '1': // bullet
+            g_gameState.currentWeapon = ProjectileType::Bullet;
+            break;
+
+        case '2': // grenade
+            g_gameState.currentWeapon = ProjectileType::Grenade;
+            break;
+
+        case 'n': // new round
+            gamestate_reset(g_gameState, g_target);
+            g_projectile.active = false;
+            break;
+    }
+
+    glutPostRedisplay();
+}
---
+++ src/Input.h
@@ -0,0 +1,5 @@
+#pragma once
+
+// Registered directly as freeglut callbacks; signatures must match exactly.
+void input_passive_motion(int x, int y);
+void input_key_up(unsigned char key, int x, int y);
